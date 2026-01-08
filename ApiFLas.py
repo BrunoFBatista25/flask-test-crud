94976669f24d04d6edba0c20c0ca1a82b7ebc9e6
Bruno Felipe <bf250000@gmail.com>
2026-01-07 23:20:07 -0300

    Add files via upload
    Adicionando arquivo poetry no Repositório.

---
+++ ApiFLas.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "hello at Flask"
+
+@app.route("/about")
+def about():
+    return "About Flask"
+
+@app.route("/contact")
+def contact():
+    return "Contact Flask"
+
+if __name__ == "__main__":
+    app.run(debug=True)
